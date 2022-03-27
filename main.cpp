19c7a7236e044e64795efa8022bc12dcb29e0bfe
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2022-03-28 00:22:20 +0430

    add comments

--- main.cpp
+++ main.cpp
@@ -16,16 +16,22 @@ int main()
     return 0;
 }
 
+// Number of ways to make N with the sum of the values of the coins
 int ways_count(int coins[], int m, int n)
 {
+    // do not include any coin
     if (n == 0)
         return 1;
 
+    // n must be greater than or equal to 1
     if (n < 0)
         return 0;
 
+
+    // no solution exist
     if (m <= 0)
         return 0;
 
+    // ways_count is sum of solutions (i)
     return ways_count(coins, m - 1, n) + ways_count(coins, m, n - coins[m - 1]);
 }
