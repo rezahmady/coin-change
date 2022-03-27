27a7e50f4aa23d167134a18b1023dc970627736c
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2022-03-28 00:09:56 +0430

    get N number from console input

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    int coins[] = { 1, 2, 5, 10, 50 };
+    int coins_size = sizeof(coins) / sizeof(coins[0]);
+
+    cout << "Coins = 1, 2, 5, 10, 50" << endl;
+    cout << "Enter N (number) :" << endl;
+    cin >> n ;
+
     return 0;
 }
